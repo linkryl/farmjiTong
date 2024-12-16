941232260a7681352d5ceeea536e814bf6213563
No-99-Tongji <3167937401@qq.com>
2024-12-16 12:18:51 +0800

    修复了SocialInfo的BUG

--- test/Classes/SocialInfo.h
+++ test/Classes/SocialInfo.h
@@ -1,8 +1,7 @@
 #pragma once
 #include <string>
+#include <vector>
 #include <map>
-#include "cocos2d.h"
-using namespace cocos2d;
 // 委托结构
 struct Entrust
 {
@@ -27,26 +26,26 @@ private:
 	// 和各个NPC的好感度
 	std::map<std::string, int> favorabilities;
 	// 各个NPC提出的委托
-	Vector<Entrust> entrusts;
+	std::vector<Entrust> entrusts;
 	// NPC的名字
-	Vector<std::string> NPC_names;
+	std::vector<std::string> NPC_names;
 public:
 	SocialInfo() {}
 	void add_NPC(const std::string& NPC_name)
 	{
-		NPC_names.pushBack(NPC_name);
+		NPC_names.push_back(NPC_name);
 		favorabilities[NPC_name] = 0;
 	}
-	void add_NPCs(const Vector<std::string> NPC_names_)
+	void add_NPCs(const std::vector<std::string> NPC_names_)
 	{
 		for (const auto& name : NPC_names_)
 			add_NPC(name);
 	}
 	void add_entrust(const Entrust& entrust)
 	{
-		entrusts.pushBack(entrust);
+		entrusts.push_back(entrust);
 	}
-	void add_entrusts(const Vector<Entrust> entrusts_)
+	void add_entrusts(const std::vector<Entrust> entrusts_)
 	{
 		for (const auto& entrust : entrusts_)
 			add_entrust(entrust);
@@ -65,10 +64,11 @@ public:
 	}
 	void solve_entrust(int id)
 	{
-		for (const auto& entrust : entrusts)
+		for (auto it = entrusts.begin(); it != entrusts.end(); )
 		{
+			const auto& entrust = *it;
 			if (entrust.id == id)
-				entrusts.eraseObject(entrust);
+				entrusts.erase(it);
 		}
 	}
 };
