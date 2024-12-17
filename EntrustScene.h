2ee2a8289bc9b71556b1d5c0082e5bd398427e32
毛韩宇 <123616490+linkryl@users.noreply.github.com>
2024-12-17 15:49:49 +0800

    新的ui

--- EntrustScene.h
+++ EntrustScene.h
@@ -1,19 +1,19 @@
 #pragma once
-#ifndef _COOPSCENE_H_
-#define _COOPSCENE_H_
+#ifndef _ENTRUSTSCENE_H_
+#define _ENTRUSTSCENE_H_
 #include "cocos2d.h"
 
 #include "SocialInfo.h"
 
 USING_NS_CC;
 
-class CoopScene : public cocos2d::Scene
+class EntrustScene : public Scene
 {
 public:
   static Scene* createScene();
 
   virtual bool init();
 
-  CREATE_FUNC(CoopScene);
+  CREATE_FUNC(EntrustScene);
 };
 #endif
