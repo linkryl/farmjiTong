88bfa52887b83f03df6ba2296506cf686e67e99f
No-99-Tongji <3167937401@qq.com>
2024-12-22 20:18:49 +0800

    玩家和背包场景的更新

--- BagScene.h
+++ BagScene.h
@@ -1,16 +1,16 @@
 #pragma once
 #include "cocos2d.h"
-
 #include "BackpackManager.h"
+#include "Skill.h" // 添加 Skill 头文件
 
 USING_NS_CC;
 
 class BagScene : public Scene
 {
 public:
-  static Scene* createScene();
+    static Scene* createScene();
 
-  virtual bool init();
+    virtual bool init();
 
-  CREATE_FUNC(BagScene);
+    CREATE_FUNC(BagScene);
 };
